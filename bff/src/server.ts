b51dc5389d4eee788f2b0e74b5f04c40298e3a83
isaac <iisrael.afv@gmail.com>
2024-07-03 19:23:27 -0300

    removing some comments

--- bff/src/server.ts
+++ bff/src/server.ts
@@ -2,7 +2,6 @@ import dotenv from 'dotenv';
 import path from 'path';
 import app from './app';
 
-// Carregar variáveis de ambiente do arquivo .env
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 const PORT = process.env.BFF_PORT || 5000;
